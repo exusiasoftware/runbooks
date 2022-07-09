5b82a1945a417fbbebec06c379c49deed71eb0f1
Brian Rahming <admin@rahmingtech.com>
2022-07-09 13:28:04 -0700

    added vmstop

---
+++ EventGridIntegration.ps1
@@ -0,0 +1,8 @@
+param(
+[object]$WebhookData
+) $
+WebhookData.WebhookName |
+ConvertFrom-Json |
+Format-List *
+$WebhookData.RequestHeaders | ConvertFrom-Json | Format-List *
+$WebhookData.RequestBody | ConvertFrom-Json | Format-List *
